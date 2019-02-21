5b5fe5ffd1419ad43cf87c615735cc840d37dc62
This PC <abhishekchahar260@gmail.com>
2019-02-21 11:26:56 +0530

    add to snack bar

--- app/src/main/java/gla/gla1/MainActivity.java
+++ app/src/main/java/gla/gla1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Abhishek singh",
+                Snackbar.make(view, "Abhishek singh mca",
                         Snackbar.LENGTH_LONG)
                         .setAction("ok", null).show();
             }
